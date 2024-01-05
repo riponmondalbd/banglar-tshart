2868413b2bbf09ea4b027648962c8763b5707ca7
Ripon Mondal <riponmondal149@gmail.com>
2024-01-05 19:52:34 +0600

    setup react router and local api mean tshirt json create and also create a nav

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home/Home.jsx'
+import Main from './components/Layout/Main.jsx'
+import OrderReview from './components/OrderReview/OrderReview.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        loader: () => fetch('tshirt.json'),
+      },
+      {
+        path: 'review',
+        element: <OrderReview></OrderReview>,
+      }
+    ]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
